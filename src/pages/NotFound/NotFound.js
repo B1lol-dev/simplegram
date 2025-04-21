579d3afcc079b360a808c1d8d3c7b1ea3e33d0a8
B1lol-dev <sardorovichgamer@gmail.com>
2025-04-22 04:38:03 +0500

    fix 404 page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,9 +1,9 @@
 export const NotFound = () => {
   return /*html*/ `
-    <div class="h-[100vh] w-full flex justify-center items-center flex-col dark:bg-hg-black">
-      <h1 class="text-7xl font-bold dark:text-hg-white">404</h1>
-      <p class="text-hg-gray">the page you looking for is not found :(</p>
-      <a href="/"><button type="button" class="bg-hg-aqua p-2 text-hg-white mt-3 rounded-lg">Go to Home</button></a>
+    <div class="h-[100vh] w-full flex justify-center items-center flex-col dark:bg-sg-black">
+      <h1 class="text-7xl font-bold dark:text-sg-white">404</h1>
+      <p class="text-sg-gray">the page you looking for is not found :(</p>
+      <a href="/"><button type="button" class="bg-sg-aqua p-2 text-sg-white mt-3 rounded-lg">Go to Home</button></a>
     </div>
     `;
 };
